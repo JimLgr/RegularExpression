8d14f68572efdcce231b8ea46f971169b49f66f6
Jim Legrandhenri <jimlegrandhenri007@outlook.com>
2022-08-05 14:54:25 +0200

    retirer la première et la dernière lettre d'une phrase

---
+++ ExoR/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+Console.WriteLine("Tappez une phrase");
+string inPhrase2 = Console.ReadLine();
+if (inPhrase2.Length > 1)
+{ 
+    Console.WriteLine(inPhrase2[1..^1]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
